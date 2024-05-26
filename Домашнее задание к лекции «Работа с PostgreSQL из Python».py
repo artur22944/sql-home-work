1aee7efb6f793e3e7b34cacacf0e0d357d226a7e
artur test <artur22944@gmail.com>
2024-05-26 15:16:37 +0300

    Third commit

--- Домашнее задание к лекции «Работа с PostgreSQL из Python».py
+++ Домашнее задание к лекции «Работа с PostgreSQL из Python».py
@@ -3,88 +3,143 @@ import psycopg2
 
 # Функция, создающая структуру БД (таблицы).
 def create_db(cur):
-    cur.execute("""
-    CREATE TABLE if not exists BASE_CLIENTS(
+    cur.execute(
+        """
+    CREATE TABLE IF NOT EXISTS base_client(
         id SERIAL PRIMARY KEY,
         first_name VARCHAR(20) NOT NULL,
         last_name VARCHAR(20) NOT NULL,
-        email VARCHAR(30) NOT NULL UNIQUE,
-        phone VARCHAR(20)
-    );
-    """)
-    print(cur.fetchall)
+        email VARCHAR(30) NOT NULL UNIQUE
+    );"""
+    )
+
+    cur.execute(
+        """
+    CREATE TABLE IF NOT EXISTS base_client_phone(
+        client_id INTEGER REFERENCES base_client(id),
+        phone VARCHAR(12) UNIQUE
+    );"""
+    )
 
 
 # Функция, позволяющая добавить нового клиента.
-def add_client(cur, first_name, last_name, email, phones=None):
-    cur.execute("""
-    INSERT INTO BASE_CLIENTS(
+def add_client(cur, id, first_name, last_name, email, phone=None):
+    cur.execute(
+        """
+    INSERT INTO base_client(
+        id,
         first_name,
         last_name,
-        email,
+        email)
+        VALUES(%s, %s, %s, %s);""",
+        (id, first_name, last_name, email),
+    )
+
+    cur.execute(
+        """
+    INSERT INTO base_client_phone(
+        client_id,
         phone)
-        VALUES(%s, %s, %s, %s);""", (first_name, last_name, email, phones))
-    print(cur.fetchall)
+        VALUES(%s, %s);""",
+        (id, phone),
+    )
 
 
 # Функция, позволяющая добавить телефон для существующего клиента.
 def add_phone(cur, client_id, phone):
-    cur.execute("""
-    UPDATE BASE_CLIENTS SET phone = %s WHERE id = %s;""", (phone, client_id))
-    print(cur.fetchall)
+    cur.execute(
+        """
+    INSERT INTO base_client_phone(
+        client_id,
+        phone)
+        VALUES(%s, %s);""",
+        (client_id, phone),
+    )
 
 
 # Функция, позволяющая изменить данные о клиенте.
-def change_client(cur, client_id, first_name=None, last_name=None, email=None, phones=None):
-    cur.execute("""
-    UPDATE BASE_CLIENTS SET first_name = %s, last_name = %s, email = %s, phone = %s WHERE id = %s;""", (first_name, last_name, email, phones, client_id))
-    cur.execute("""SELECT * FROM BASE_CLIENTS;""")
-    print(cur.fetchall())
+def change_client(cur, client_id, first_name, last_name, email, phone):
+    cur.execute(
+        """
+    UPDATE base_client SET
+        first_name=%s, last_name=%s, email=%s WHERE id=%s;""",
+        (first_name, last_name, email, client_id),
+    )
+
+    cur.execute(
+        """
+    UPDATE base_client_phone SET phone=%s WHERE client_id=%s;""",
+        (phone, client_id),
+    )
 
 
 # Функция, позволяющая удалить телефон для существующего клиента.
-def delete_phone(cur, phone):
-    conn.execute("""DELETE FROM BASE_CLIENTS WHERE phone=%s;""", (phone,))
-    print(cur.fetchall())
+def delete_phone(cur, client_id, phone):
+    cur.execute(
+        """
+    DELETE FROM base_client_phone WHERE client_id = %s AND phone = %s;""",
+        (client_id, phone),
+    )
 
 
 # Функция, позволяющая удалить существующего клиента.
 def delete_client(cur, client_id):
-    cur.execute("""
-    DELETE FROM BASE_CLIENTS WHERE id = %s;""", (client_id, ))
-    cur.execute("""SELECT * FROM BASE_CLIENTS;""")
-    print(cur.fetchall())
+    cur.execute(
+        """
+    DELETE FROM base_client_phone WHERE client_id = %s;""",
+        (client_id,),
+    )
+
+    cur.execute(
+        """
+    DELETE FROM base_client WHERE id = %s;""",
+        (client_id,),
+    )
 
 
 # Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
 def find_client(cur, first_name=None, last_name=None, email=None, phone=None):
-    cur.execute("""
-    SELECT * FROM BASE_CLIENTS WHERE first_name = %s OR last_name = %s OR email = %s OR phone = %s;""", (first_name, last_name, email, phone))
+    cur.execute(
+        """
+    SELECT * FROM base_client
+    JOIN base_client_phone ON base_client.id = base_client_phone.client_id
+    WHERE (first_name = %(first_name)s or %(first_name)s IS NULL)
+    AND (last_name = %(last_name)s or %(last_name)s IS NULL)
+    AND (email = %(email)s or %(email)s IS NULL)
+    AND (phone = %(phone)s or %(phone)s IS NULL);""",
+        {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "phone": phone,
+        },
+    )
+
     print(cur.fetchall())
 
 
 if __name__ == "__main__":
-    with psycopg2.connect(database="netology_db",
-                          user="postgres",
-                          password="ugohos12") as conn:
+    with psycopg2.connect(
+        database="netology_db", user="postgres", password="ugohos12"
+    ) as conn:
         with conn.cursor() as cur:
 
             # create_db(cur)
 
-            # add_client(cur, "Иван", "Иванов", "ivan@ya.ru", "123456789")
-            # add_client(cur, "Петр", "Петров", "petr@ya.ru", "987654321")
-            # add_client(cur, "Сидор", "Сидоров", "sidor@ya.ru")
-            # add_client(cur, "Вася", "Васильев", "vasya@ya.ru")
+            # add_client(cur, 1, "Иван", "Иванов", "ivan@ya.ru", "123456789")
+            # add_client(cur, 2, "Петр", "Петров", "petr@ya.ru", "987654321")
+            # add_client(cur, 3, "Сидор", "Сидоров", "sidor@ya.ru")
+            # add_client(cur, 4, "Вася", "Васильев", "vasya@ya.ru")
 
-            # add_phone(cur, 1, "684798516")
-            # add_phone(cur, 3, "684168668")
+            # add_phone(cur, 3, "684798516")
+            # add_phone(cur, 4, "684168668")
 
-            # change_client(cur, 1, first_name="Олег", last_name="Олегов", email="oleg@ya.ru", phones="165165165")
+            # change_client(cur, 1, "Олег", "Иванов", "ivan@ya.ru", "123456789")
 
-            # delete_phone(cur, "165165165")
+            # delete_phone(cur, 3, "684798516")
 
             # delete_client(cur, 2)
 
-            # find_client(cur, first_name="Вася")
+            find_client(cur, "Вася")
 
     conn.close()
