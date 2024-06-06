6adde9e5cb13c36740333cb95782faea9ad8c312
artur test <artur22944@gmail.com>
2024-06-06 17:23:21 +0300

    ORM first commit

--- Домашнее задание к лекции «Python и БД. ORM»/main.py
+++ Домашнее задание к лекции «Python и БД. ORM»/main.py
@@ -10,48 +10,54 @@ login = input("Введите логин: ")
 password = input("Введите пароль: ")
 database = input("Введите название базы данных: ")
 
+
 DNS = f"postgresql://{login}:{password}@localhost:5432/{database}"
 engine = sq.create_engine(DNS)
+create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
-create_tables(engine)
+
 
 with open("fixtures/tests_data.json", "r") as fd:
     data = json.load(fd)
 
-    for record in data:
-        model = {
-            "publisher": Publisher,
-            "shop": Shop,
-            "book": Book,
-            "stock": Stock,
-            "sale": Sale,
-        }[record.get("model")]
-        session.add(model(id=record.get("pk"), **record.get("fields")))
-    session.commit()
+for record in data:
+    model = {
+        "publisher": Publisher,
+        "shop": Shop,
+        "book": Book,
+        "stock": Stock,
+        "sale": Sale,
+    }[record.get("model")]
+    session.add(model(id=record.get("pk"), **record.get("fields")))
+session.commit()
 
-input_publisher = input("Введите имя издателя: ")
-if input_publisher.isdigit():
-    input_publisher = int(input_publisher)
-    publisher = (
-        session.query(Shop)
-        .join(Stock.shops)
+
+def get_shops(name):
+    query = (
+        session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)
+        .select_from(Shop)
+        .join(Stock)
         .join(Book)
         .join(Publisher)
-        .filter(Publisher.id == input_publisher)
-        .all()
+        .join(Sale)
     )
-else:
-    publisher = (
-        session.query(Shop)
-        .join(Stock.shops)
-        .join(Book)
-        .join(Publisher)
-        .filter(Publisher.name == input_publisher)
-        .all()
-    )
-print(publisher)
+    if name.isdigit():
+        name = int(name)
+        result = query.filter(Publisher.id == name).all()
+    else:
+        result = query.filter(Publisher.name == name).all()
+    for title, name, price, date_sale in result:
+        print(
+            f"{title: <40} | {name: <10} | {price: <8} | {date_sale.strftime('%d.%m.%Y')}"
+        )
+
+
+if __name__ == "__main__":
+    name = input("Введите имя издателя: ")
+    get_shops(name)
+
 
 drop_tables(engine)
 
